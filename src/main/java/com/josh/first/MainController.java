3136d450eea6df6049ec1b57dffca76ced73b416
overbejt <overbejt@miamioh.edu>
2020-06-12 00:50:50 -0400

    Woring on the form

--- src/main/java/com/josh/first/MainController.java
+++ src/main/java/com/josh/first/MainController.java
@@ -1,13 +1,16 @@
-package com.josh.first.controllers;
+package com.josh.first;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
 	@GetMapping("/register")
-	public String showForm() {
+	public String showForm(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
 		return "register_form";
 	}
 }  // End of the 'MainController' class
